190dd9bc6abf00eac422732d81da4cd04507bcca
samuelloganbjss <samuel.logan@bjss.com>
2024-10-24 10:47:56 +0100

    - Added test framework and test file

---
+++ LBTTCalculatorApp/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'react-native',
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    transformIgnorePatterns: [
+      'node_modules/(?!(react-native|@react-native|@react-native-community)/)',
+    ],
+  };
+  
