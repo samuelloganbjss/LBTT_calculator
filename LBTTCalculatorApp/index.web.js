7752d556e1ba0a397381d83553e0f5718ae032f1
samuelloganbjss <samuel.logan@bjss.com>
2024-10-24 10:32:53 +0100

    - Finished setup

--- LBTTCalculatorApp/index.web.js
+++ LBTTCalculatorApp/index.web.js
@@ -1,9 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import appConfig from './app.json'; 
 import { render } from 'react-dom';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appConfig.name, () => App); 
 
 const rootTag = document.getElementById('app-root');
-AppRegistry.runApplication(appName, { initialProps: {}, rootTag });
+AppRegistry.runApplication(appConfig.name, { initialProps: {}, rootTag });
--- LBTTCalculatorApp/webpack.config.js
+++ LBTTCalculatorApp/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development', 
   entry: './index.web.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
