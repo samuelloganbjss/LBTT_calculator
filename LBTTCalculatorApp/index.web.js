868d9e1629aab326564af2db04c86cbe446a0c16
samuelloganbjss <samuel.logan@bjss.com>
2024-10-24 13:16:04 +0100

    -reverted back to old code

--- LBTTCalculatorApp/index.web.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import appConfig from './app.json'; 
-import { render } from 'react-dom';
-
-AppRegistry.registerComponent(appConfig.name, () => App); 
-
-const rootTag = document.getElementById('app-root');
-AppRegistry.runApplication(appConfig.name, { initialProps: {}, rootTag });
